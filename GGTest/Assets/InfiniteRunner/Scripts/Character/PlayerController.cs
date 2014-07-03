06b416d153280342d9f5d526186800d800ec93b9
Gerzan <gerzan.munguia@gmail.com>
2014-07-03 17:11:24 -0600

    Back Ground, Movement and Character

---
+++ GGTest/Assets/InfiniteRunner/Scripts/Character/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2f;
+
+	void Update() {
+
+		Vector3 dir = Vector3.zero;
+		dir.y = Input.acceleration.x;
+		dir.z = 0f;
+		dir.x = 0f;//Input.acceleration.x;
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+		
+		dir *= Time.deltaTime;
+		transform.Translate(dir * speed);
+	
+	}
+	
+	
+	
+	
+}
---
+++ GGTest/Assets/InfiniteRunner/Scripts/Character/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float speed = 1000f;
+	// Update is called once per frame
+	void Update () {
+	
+		this.transform.Rotate (new Vector3 (0, 0, -(Time.deltaTime * speed)));
+
+	}
+}
