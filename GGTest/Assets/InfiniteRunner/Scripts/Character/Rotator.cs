132c76038fe8996a49cd79ac3f27790462aac48f
Gerzan <gerzan.munguia@gmail.com>
2014-07-14 11:34:11 -0600

    Shuriken Added and rotator

---
+++ GGTest/Assets/InfiniteRunner/Scripts/Character/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed = 5;
+	
+
+	void Update () {
+	
+		transform.Rotate(new Vector3(0f,0f, Time.deltaTime * speed));
+
+	}
+}
